47cf93bfa681fbd3075689acff729746c6ac99a3
vinaysu <suddakuntivinay7877@gmail.com>
2024-05-07 12:51:59 +0530

    initial commit for backend

---
+++ model.js
@@ -0,0 +1,17 @@
+const mongoose = require('mongoose');
+
+// Define a schema for the data
+const FormDataSchema = new mongoose.Schema({
+    enquirerType: { type: String, required: true },
+    enquirerName: { type: String, required: true },
+    enquirerMobile: { type: String, required: true },
+    enquirerWhatsapp: { type: String, required: true },
+    productEnquired: { type: String, required: true },
+    studentsClass: { type: String, required: true },
+    studentsBoard: { type: String, required: true },
+});
+
+// Create a model based on the schema
+const FormDataModel = mongoose.model('FormData', FormDataSchema);
+
+module.exports = FormDataModel;
